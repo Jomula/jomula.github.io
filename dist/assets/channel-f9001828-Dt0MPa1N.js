1587fd55b25e5bf651728d58453f5cba97816150
Jonas Müller-Laackman <jonas.mueller-laackman@sub.uni-hamburg.de>
2024-10-14 14:24:46 +0200

    clean up & add new presi structure

--- dist/assets/channel-f9001828-Dt0MPa1N.js
+++
@@ -1,1 +0,0 @@
-import{aG as o,aH as r}from"./md-BLpqUY53.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
--- dist/assets/clone-9ea6bfeb-Ff0jSpLn.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./graph-fe24fab6-Sh1oDEiZ.js";var e=4;function a(o){return r(o,e)}export{a as c};
